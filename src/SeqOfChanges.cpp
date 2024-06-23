46783806f4fca4eef4f86e1286e6a836fec91836
Dylan Gardham <message4dylan@gmail.com>
2024-06-23 13:33:06 +0100

    change order and convert params to enums

--- src/SeqOfChanges.cpp
+++ src/SeqOfChanges.cpp
@@ -3,14 +3,7 @@
 
 struct SeqOfChanges : Module {
 	enum ParamId {
-		LI_PARAM,
-		XUN_PARAM,
-		KUN_PARAM,
-		ZHEN_PARAM,
-		DUI_PARAM,
-		GEN_PARAM,
-		QIAN_PARAM,
-		KAN_PARAM,
+		ENUMS(PROBABILITIES, 8),
 		PARAMS_LEN
 	};
 	enum InputId {
@@ -23,28 +16,33 @@ struct SeqOfChanges : Module {
 		OUTPUTS_LEN
 	};
 	enum LightId {
-		LIGHT_1_LIGHT,
-		LIGHT_2_LIGHT,
-		LIGHT_3_LIGHT,
+		ENUMS(LIGHTS, 3),
 		LIGHTS_LEN
 	};
 
+	dsp::TSchmittTrigger<float> reset;
+	int state = 0; // first three bits represent the previous three outputs
+
+
 	SeqOfChanges() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(LI_PARAM, 0.f, 1.f, 0.f, "Li");
-		configParam(XUN_PARAM, 0.f, 1.f, 0.f, "Xun");
-		configParam(KUN_PARAM, 0.f, 1.f, 0.f, "Kun");
-		configParam(ZHEN_PARAM, 0.f, 1.f, 0.f, "Zhen");
-		configParam(DUI_PARAM, 0.f, 1.f, 0.f, "Dui");
-		configParam(GEN_PARAM, 0.f, 1.f, 0.f, "Gen");
-		configParam(QIAN_PARAM, 0.f, 1.f, 0.f, "Qian");
-		configParam(KAN_PARAM, 0.f, 1.f, 0.f, "Kan");
+		configParam(PROBABILITIES + 0, 0.f, 1.f, 0.f, "Kun");
+		configParam(PROBABILITIES + 1, 0.f, 1.f, 0.f, "Dui");
+		configParam(PROBABILITIES + 2, 0.f, 1.f, 0.f, "Li");
+		configParam(PROBABILITIES + 3, 0.f, 1.f, 0.f, "Zhen");
+		configParam(PROBABILITIES + 4, 0.f, 1.f, 0.f, "Xun");
+		configParam(PROBABILITIES + 5, 0.f, 1.f, 0.f, "Kan");
+		configParam(PROBABILITIES + 6, 0.f, 1.f, 0.f, "Gen");
+		configParam(PROBABILITIES + 7, 0.f, 1.f, 0.f, "Qian");
 		configInput(CLK_INPUT, "Clock");
 		configInput(RESET_INPUT, "Reset");
 		configOutput(OUT_OUTPUT, "Output");
 	}
 
 	void process(const ProcessArgs& args) override {
+		if (reset.process(inputs[RESET_INPUT].getVoltage(), 0.1f, 1.f)) {
+			step = 0;
+		}
 	}
 };
 
@@ -59,23 +57,23 @@ struct SeqOfChangesWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(30.688, 74.21)), module, SeqOfChanges::LI_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(14.949, 83.536)), module, SeqOfChanges::XUN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.101, 83.598)), module, SeqOfChanges::KUN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.252, 97.134)), module, SeqOfChanges::ZHEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(52.708, 97.134)), module, SeqOfChanges::DUI_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.212, 110.773)), module, SeqOfChanges::GEN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.296, 110.602)), module, SeqOfChanges::QIAN_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(30.688, 120.694)), module, SeqOfChanges::KAN_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.101, 83.598)), module, SeqOfChanges::PROBABILITIES + 0));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(52.708, 97.134)), module, SeqOfChanges::PROBABILITIES + 1));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(30.688, 74.21)), module, SeqOfChanges::PROBABILITIES + 2));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.252, 97.134)), module, SeqOfChanges::PROBABILITIES + 3));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(14.949, 83.536)), module, SeqOfChanges::PROBABILITIES + 4));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(30.688, 120.694)), module, SeqOfChanges::PROBABILITIES + 5));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.212, 110.773)), module, SeqOfChanges::PROBABILITIES + 6));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(46.296, 110.602)), module, SeqOfChanges::PROBABILITIES + 7));
 
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(16.458, 25.393)), module, SeqOfChanges::CLK_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(30.828, 25.393)), module, SeqOfChanges::RESET_INPUT));
 
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(44.742, 25.305)), module, SeqOfChanges::OUT_OUTPUT));
 
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(30.48, 38.308)), module, SeqOfChanges::LIGHT_1_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(30.48, 49.802)), module, SeqOfChanges::LIGHT_2_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(30.48, 61.296)), module, SeqOfChanges::LIGHT_3_LIGHT));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(30.48, 38.308)), module, SeqOfChanges::LIGHTS + 0));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(30.48, 49.802)), module, SeqOfChanges::LIGHTS + 1));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(30.48, 61.296)), module, SeqOfChanges::LIGHTS + 2));
 	}
 };
 
